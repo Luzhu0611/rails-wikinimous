6a5ee74399b8f961e1b475584d62e4ebe6c52b20
Lu Zhu <luzhu000@gmail.com>
2019-07-02 20:23:04 +0200

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(
+    title: Faker::TvShows::HowIMetYourMother.catch_phrase,
+    content: Faker::TvShows::HowIMetYourMother.quote
+  )
+end
